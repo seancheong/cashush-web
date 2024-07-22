fa12ebd994e9e56acd3727d2fa70ef1894fcabaf
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2024-07-22 21:58:42 +0900

    Integrate with Framer motion for animation (#22)

--- src/app/[locale]/_components/Hero.tsx
+++ src/app/[locale]/_components/Hero.tsx
@@ -1,3 +1,4 @@
+import { Reveal } from '@/components/Reveal';
 import { Button } from '@/components/ui/button';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
@@ -10,25 +11,33 @@ export const Hero = () => {
     <section className="mb-16">
       <div className="xl:flex xl:justify-between xl:gap-5">
         <div className="mb-5 rounded-lg bg-secondary px-4 py-5 md:py-12 xl:mb-0 xl:flex xl:h-auto xl:w-1/2 xl:flex-col xl:justify-center">
-          <h1 className="mb-6 text-5xl font-medium md:text-8xl">
-            {t('hero.title')}
-          </h1>
+          <Reveal direction="bottom">
+            <h1 className="mb-6 text-5xl font-medium md:text-8xl">
+              {t('hero.title')}
+            </h1>
+          </Reveal>
 
-          <p className="mb-10 text-lg">{t('hero.subtitle')}</p>
+          <Reveal direction="bottom" delay={0.6}>
+            <p className="mb-10 text-lg">{t('hero.subtitle')}</p>
+          </Reveal>
 
-          <Button className="w-full md:w-fit" asChild>
-            <Link href="/#features">{t('hero.start')}</Link>
-          </Button>
+          <Reveal direction="bottom" delay={0.8}>
+            <Button className="w-full md:w-fit" asChild>
+              <Link href="/#features">{t('hero.start')}</Link>
+            </Button>
+          </Reveal>
         </div>
 
         <div className="relative h-[500px] xl:h-auto xl:w-1/2">
-          <Image
-            src="/images/hero.svg"
-            alt={t('hero.image')}
-            priority
-            fill
-            className="rounded-lg object-cover"
-          />
+          <Reveal delay={0.8}>
+            <Image
+              src="/images/hero.svg"
+              alt={t('hero.image')}
+              priority
+              fill
+              className="rounded-lg object-cover"
+            />
+          </Reveal>
         </div>
       </div>
     </section>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import { Button } from '@/components/ui/button';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
+import { Reveal } from '../Reveal';
+
 const navItems = [
   {
     title: 'features',
@@ -17,28 +19,30 @@ export const Header = () => {
   const t = useTranslations('Common');
 
   return (
-    <header className="mb-6 border-b-[0.5px] border-neutral-200">
-      <nav className="flex items-center justify-between px-4 py-4 md:px-8">
-        <Link href="/" className="text-[1.75rem] font-bold text-heading">
-          {t('brand')}
-        </Link>
+    <Reveal direction="top" delay={0.6}>
+      <header className="mb-6 border-b-[0.5px] border-neutral-200">
+        <nav className="flex items-center justify-between px-4 py-4 md:px-8">
+          <Link href="/" className="text-[1.75rem] font-bold text-heading">
+            {t('brand')}
+          </Link>
 
-        <ul className="hidden w-1/3 items-center justify-between md:flex">
-          {navItems.map((item) => (
-            <li key={item.title}>
-              <Link href={item.href}>{t(`nav.${item.title}`)}</Link>
-            </li>
-          ))}
-        </ul>
+          <ul className="hidden w-1/3 items-center justify-between md:flex">
+            {navItems.map((item) => (
+              <li key={item.title}>
+                <Link href={item.href}>{t(`nav.${item.title}`)}</Link>
+              </li>
+            ))}
+          </ul>
 
-        <Button className="md:hidden" asChild>
-          <Link href="/#contact">{t('buttons.contact')}</Link>
-        </Button>
+          <Button className="md:hidden" asChild>
+            <Link href="/#contact">{t('buttons.contact')}</Link>
+          </Button>
 
-        <Button className="hidden md:flex" asChild>
-          <Link href="/#features">{t('buttons.start')}</Link>
-        </Button>
-      </nav>
-    </header>
+          <Button className="hidden md:flex" asChild>
+            <Link href="/#features">{t('buttons.start')}</Link>
+          </Button>
+        </nav>
+      </header>
+    </Reveal>
   );
 };
